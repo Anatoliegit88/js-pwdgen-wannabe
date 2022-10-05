cf1f7b11880a16431712a5ac7278c278217d7fd1
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-05 15:15:11 +0200

    first-js.

---
+++ script.js
@@ -0,0 +1,13 @@
+// Ciao ragazzi,
+// esercizio di oggi: (insicurissimo) password generator
+// nome repo: js-pwdgen-wannabe
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+const nameUser = prompt("Whats your name?");
+const surnameUser = prompt("Whats your surname?");
+const colorUser = prompt("Whats your favorite color?");
+const passwordUser = nameUser + surnameUser + colorUser;
+document.getElementById;
+"answer".innerHTML = answer;
